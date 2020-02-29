843181ac8756989b3031f3f24c48b2ea4f28d7e4
karouia <alaedinekarouia@gmail.com>
2020-02-29 15:51:36 +0100

    cam libre  PLUS  details

--- jsbaby/src/js/Player.js
+++ jsbaby/src/js/Player.js
@@ -10,12 +10,9 @@ Player = function(game, canvas) {
 Player.prototype = {
     _initCamera : function(scene, canvas) {
         // On crée la caméra
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+        var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, true);
 
-        // On demande à la caméra de regarder au point zéro de la scène
-        this.camera.setTarget(BABYLON.Vector3.Zero());
-
-        // On affecte le mouvement de la caméra au canvas
-        this.camera.attachControl(canvas, true);
     }
 };
